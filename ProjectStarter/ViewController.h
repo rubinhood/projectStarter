52ce49852ca9d39fbb32aafd1126863aa5773016
Sarfaraz Halai <rubin@grr.la>
2018-08-07 01:50:48 +0530

    first commit

---
+++ ProjectStarter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectStarter
+//
+//  Created by Sarfaraz Halai on 2018-08-06.
+//  Copyright © 2018 Sarfaraz Halai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectStarter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectStarter
+//
+//  Created by Sarfaraz Halai on 2018-08-06.
+//  Copyright © 2018 Sarfaraz Halai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
